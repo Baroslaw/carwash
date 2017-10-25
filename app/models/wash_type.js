7a8f508a37517627f7dc2164ded9685ce082e584
Adam Brodziak <adambro@o2.pl>
2017-10-25 23:24:38 +0200

    Simplify return value

--- app/models/wash_type.js
+++ app/models/wash_type.js
@@ -6,13 +6,7 @@ class WashTypeModel {
         var result = await global.DbExecute(
             'SELECT * FROM `wash_types` WHERE `active` <> 0 ORDER BY `order_number`'
         );
-
-        if (result && result.length > 0) {
-            return result;
-        }
-        else {
-            return [];
-        }
+        return result || [];
     }
 
     static async GetWashTypeById(id) {
